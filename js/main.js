03494915ec6644cb1e4ab4edf368508beb36ddec
hamza202 <k8668975@gmail.com>
2020-07-16 03:05:39 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -10,6 +10,47 @@ $('.fave-button').on('click', function (e) {
     e.stopPropagation();
     e.preventDefault();
 });
-$('.product-cart-button').on('click', function (e){
+$('.product-cart-button').on('click', function (e) {
     e.preventDefault();
 });
+if($('#slick1').length) {
+    $('#slick1').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        rtl: true,
+        rows: 2,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+        ]
+    });
+}
+
+if($('.slider').length){
+    $('.prev-button').click(function (e) {
+        e.preventDefault();
+        $(this).closest('.sliders').find('.slider-container').slick('slickNext');
+    });
+
+    $('.next-button').click(function (e) {
+        e.preventDefault();
+        $(this).closest('.sliders').find('.slider-container').slick('slickPrev');
+    });
+}
+
