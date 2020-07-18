b7bbcd023e5977a6d027d4054cf98da033aeefcd
hamza202 <k8668975@gmail.com>
2020-07-18 07:35:50 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -42,6 +42,34 @@ if($('#slick1').length) {
     });
 }
 
+if($('#slick2').length) {
+    $('#slick2').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        rtl: true,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+        ]
+    });
+}
+
 if($('.slider').length){
     $('.prev-button').click(function (e) {
         e.preventDefault();
