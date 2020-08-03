1f1a5779224f6706422d0481c290aeaae590b2c0
hamza202 <k8668975@gmail.com>
2020-08-03 21:07:59 +0300

    8/3

--- js/main.js
+++ js/main.js
@@ -114,3 +114,51 @@ $('.address-repeater').on('click', '.del-address', function (e) {
     e.preventDefault()
 });
 
+//plus-btnValue minus-btnValue
+$(document).ready(function () {
+    $(document).on('click', '.minus-btn, .plus-btn', function (e) {
+        var $this = $(e.target),
+            input = $this.parent().parent().find('.number-cunt'),
+            v = $this.hasClass('minus-btn') ? input.val() - 1 : input.val() * 1 + 1,
+            min = input.attr('data-min') ? input.attr('data-min') : 1,
+            max = input.attr('data-max') ? input.attr('data-max') : false;
+        if (v >= min) {
+            if (!max === false && v > max) {
+                return false
+            } else input.val(v);
+        }
+        e.preventDefault();
+    });
+    $(document).on('change', '.number-cunt', function (e) {
+        var input = $(e.target),
+            min = input.attr('data-min') ? input.attr('data-min') : 1,
+            max = input.attr('data-max'),
+            v = input.val();
+        if (v > max) input.val(max);
+        else if (v < min) input.val(min);
+    });
+});
+
+$(function () {
+    var charLimit = 1;
+    $(".inputs").keydown(function (e) {
+
+        var keys = [8, 9, /*16, 17, 18,*/ 19, 20, 27, 105, 104, 103, 102, 101, 100, 99, 98, 97, 96, 33, 34, 35, 36, 37, 38, 39, 40, 45, 46, 48, 144, 145];
+
+        if (e.which == 8 && this.value.length == 0) {
+            $(this).prev('.inputs').focus();
+        } else if ($.inArray(e.which, keys) >= 0) {
+            return true;
+        } else if (this.value.length >= charLimit) {
+            $(this).next('.inputs').focus();
+            return false;
+        } else if (e.shiftKey || e.which <= 48 || e.which >= 58) {
+            return false;
+        }
+    }).keyup(function () {
+        if (this.value.length >= charLimit) {
+            $(this).next('.inputs').focus();
+            return false;
+        }
+    });
+});
